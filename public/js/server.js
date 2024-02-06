aa217d9f0b4731c64971be2306d03e67a87761ff
juna-2017 <juna.okon@yahoo.com>
2024-02-05 21:08:39 -0500

    another commit to catch anything missed

--- public/js/server.js
+++ public/js/server.js
@@ -1,6 +1,11 @@
 const addCommentBtn = document.querySelector('#addCommentBtn');
 const commentTextBody = document.querySelector('#userComment');
+const submitBtn = document.querySelector('#submitBtn');
 
 addCommentBtn.addEventListener('click', function(){
     commentTextBody.classList.replace('hide', 'show');
 });
+
+submitBtn.addEventListener('click', function(){
+    
+})
